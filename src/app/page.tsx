c8c0c46d88fc6ff8d9c4addbf9522acf27051a0f
EdOsorio <106685409+EdOsorio@users.noreply.github.com>
2024-03-12 23:08:37 -0500

    using header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HeaderComponent from '../../lib/src/app/HeaderComponent';
+
 export default function Home() {
   return (
     <div>
+      <HeaderComponent />
       <h1>Submodules test page</h1>
     </div>
   );
