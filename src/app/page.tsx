98c8980874dae73aaf39553e8e4a54520971e0e1
EdOsorio <106685409+EdOsorio@users.noreply.github.com>
2024-03-12 23:13:19 -0500

    Deleting submdule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import HeaderComponent from '../../lib/src/app/HeaderComponent';
+// import HeaderComponent from '../../lib/src/app/HeaderComponent';
 
 export default function Home() {
   return (
     <div>
-      <HeaderComponent />
+      {/* <HeaderComponent /> */}
       <h1>Submodules test page</h1>
     </div>
   );
